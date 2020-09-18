455617749ce7ba09af4bd453ddad924a28b2ba5e
isfenixrojo <isfenixrojo@mail.com>
2020-09-17 19:32:11 -0500

    Se agrega componente cart.service con metodos de clear,ge y add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     TopBarComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +26,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'productos/:productoId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   providers: [],
